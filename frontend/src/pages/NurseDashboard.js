cd716ddcffbe56ac097148fbf5362ac3cae7b902
DennisAmutsa <amutsadennis@gmail.com>
2025-08-12 18:36:36 +0300

    fixed create handover button

--- frontend/src/pages/NurseDashboard.js
+++ frontend/src/pages/NurseDashboard.js
@@ -955,6 +955,14 @@ const NurseDashboard = () => {
                   Create Handover
                 </button>
               )}
+              {currentHandover && (
+                <button
+                  onClick={() => setShowShiftHandover(true)}
+                  className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition-colors"
+                >
+                  Create New Handover
+                </button>
+              )}
             </div>
           </div>
 
